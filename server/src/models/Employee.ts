a4b7e8493f7b76d30d6c78baf3d168dea9761ec8
Miraaj Kadam <miraajkadam@gmail.com>
2022-02-27 20:20:30 +0530

    Setup MongoDB

--- server/src/models/Employee.ts
+++ server/src/models/Employee.ts
@@ -1,10 +1,10 @@
-import { Schema as MongooseSchema } from 'mongoose';
-
-const Schema = MongooseSchema;
+import { model, Schema } from 'mongoose';
 
 const employeeSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
 });
 
-export default employeeSchema;
+const Employee = model('Employee', employeeSchema);
+
+export default Employee;
