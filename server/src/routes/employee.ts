c4c76eeb3d71632c4e3d5febf93c4674b90695a4
Miraaj Kadam <miraajkadam@gmail.com>
2022-03-27 16:41:29 +0530

    Cleanup REST Stuff...

--- server/src/routes/employee.ts
+++
@@ -1,6 +0,0 @@
-import express, { Router } from 'express'
-import { getAllEmployees as fetchAllEmployees } from '../controllers/employee'
-
-export const router: Router = express.Router()
-
-router.get('/', fetchAllEmployees)
